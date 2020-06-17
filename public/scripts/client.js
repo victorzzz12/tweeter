b891439c8d32133da010f0f296d5a4fd87d8a1b7
Victor Liang <victor.zw.liang@gmail.com>
2020-06-17 20:07:35 +0000

    Added many functionalities including new dynamic tweets and fetching data using ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,55 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const createTweetElement = (i) => {
+  const tweet = 
+  `<article>
+  <div class="header">
+    <span>
+      <img src="${i.user.avatars}" alt="This is a profile image"><h4>${i.user.name}</h4>
+    </span>
+    <h4 class="tag">${i.user.handle}</h4>
+  </div>
+  <div><p>${i.content.text}</p></div>
+  <div class="footer">
+    <h6>${(new Date() - i.created_at)/1000/60/60/24/365}</h6>
+    <div>
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+  </div>
+</article>`
 
+  return tweet;
+}
+
+const renderTweets = tweets => {
+  for (let i of tweets) {
+    console.log(i)
+    $("#tweet-container").append(createTweetElement(i));
+    
+  }
+}
+
+$(document).ready(() => {
+  console.log("ready!");
+  $("form").on("submit", (event) => {
+      event.preventDefault();
+      console.log("Working");
+      $.ajax({
+          url: "/tweets/",
+          method: "POST",
+          data: $("form").serialize()
+      })
+      .done(() => {
+          $("#tweet-container").empty()
+          loadTweets();
+        });
+  })
+  function loadTweets() {
+    $.ajax("/tweets/", { method: "GET" })
+    .then((data) => {
+      renderTweets(data);
+    })
+  }
+
+loadTweets();
+})
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  const $textArea = $(".new-tweet form textarea.tweet-text");
+  const $counter = $(".new-tweet form output.counter");
+  
+  $textArea.on("keyup", (function() {
+    const $this = $(this);
+    if ((140 - ($this.val().length) >= 0)) {
+      $counter.css("color", "black");
+      $counter.text(140 - ($this.val().length));
+    } else {
+      $counter.css("color", "red");
+      $counter.text(140 - ($this.val().length));
+    }
+  }));
+
+});
+
